5b9652a74d05d30770ecb2e13480e9cdcdcdff79
javier <bonaventura.javier@gmail.com>
2022-04-30 14:02:18 -0300

    Nuevo

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -28,6 +28,83 @@ module.exports = {
     res.render("electrodomesticos", { product: soloElectrosAudio });
   },
 
+  electrodomesticosCocinas: (req, res) => {
+    const soloElectros = products.filter(
+      (Electro) => Electro.categoria === "Electrodomesticos"
+    );
+    const soloElectrosCocinas = products.filter(
+        (Electro) => Electro.subcategoria === "Cocinas"
+      );
+
+    res.render("electrodomesticos", { product: soloElectrosCocinas });
+  },
+
+  electrodomesticosSecarropas: (req, res) => {
+    const soloElectros = products.filter(
+      (Electro) => Electro.categoria === "Electrodomesticos"
+    );
+    const soloElectrosSecarropas = products.filter(
+        (Electro) => Electro.subcategoria === "Secarropas"
+      );
+
+    res.render("electrodomesticos", { product: soloElectrosSecarropas });
+  },
+
+  electrodomesticosLavarropas: (req, res) => {
+    const soloElectros = products.filter(
+      (Electro) => Electro.categoria === "Electrodomesticos"
+    );
+    const soloElectrosLavarropas = products.filter(
+        (Electro) => Electro.subcategoria === "Lavarroas"
+      );
+
+    res.render("electrodomesticos", { product: soloElectrosLavarropas });
+  },
+
+  electrodomesticosEstufas: (req, res) => {
+    const soloElectros = products.filter(
+      (Electro) => Electro.categoria === "Electrodomesticos"
+    );
+    const soloElectrosEstufas = products.filter(
+        (Electro) => Electro.subcategoria === "Estufas"
+      );
+
+    res.render("electrodomesticos", { product: soloElectrosEstufas });
+  },
+
+  electrodomesticosTermotanques: (req, res) => {
+    const soloElectros = products.filter(
+      (Electro) => Electro.categoria === "Electrodomesticos"
+    );
+    const soloElectrosTermotanques = products.filter(
+        (Electro) => Electro.subcategoria === "Termotanques"
+      );
+
+    res.render("electrodomesticos", { product: soloElectrosTermotanques });
+  },
+  
+  electrodomesticosHornos: (req, res) => {
+    const soloElectros = products.filter(
+      (Electro) => Electro.categoria === "Electrodomesticos"
+    );
+    const soloElectrosHornos = products.filter(
+        (Electro) => Electro.subcategoria === "Hornos"
+      );
+
+    res.render("electrodomesticos", { product: soloElectrosHornos });
+  },
+    
+  electrodomesticosMicroondas: (req, res) => {
+    const soloElectros = products.filter(
+      (Electro) => Electro.categoria === "Electrodomesticos"
+    );
+    const soloElectrosHMicroondas = products.filter(
+        (Electro) => Electro.subcategoria === "Microondas"
+      );
+
+    res.render("electrodomesticos", { product: soloElectrosHMicroondas });
+  },
+
   electronica: (req, res) => {
     res.render("electronica");
   },
--- routes/products.js
+++ routes/products.js
@@ -5,9 +5,17 @@ const controller = require('../controllers/productsController');
 router.get('/', controller.electrodomesticos);
 router.get('/electrodomesticos', controller.electrodomesticos);
 router.get('/electrodomesticos/audio', controller.electrodomesticosAudio);
+router.get('/electrodomesticos/cocinas', controller.electrodomesticosCocinas);
+router.get('/electrodomesticos/secarropas', controller.electrodomesticosSecarropas);
+router.get('/electrodomesticos/Lavarropas', controller.electrodomesticosLavarropas);
+router.get('/electrodomesticos/Estufas', controller.electrodomesticosEstufas);
+router.get('/electrodomesticos/Termotanques', controller.electrodomesticosTermotanques);
+router.get('/electrodomesticos/Hornos', controller.electrodomesticosHornos);
+router.get('/electrodomesticos/Microondas', controller.electrodomesticosMicroondas);
 router.get('/electronica', controller.electronica);
 router.get('/muebles', controller.muebles);
 router.get('/detail', controller.detail);
 router.get("/product/:id", controller.getOne);
+router.get("/product/electrodomesticos/:id", controller.getOne);
 
 module.exports = router;
